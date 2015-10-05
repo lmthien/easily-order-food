f1b789808fecc715088da9b8c81a807d48ba53aa
lmthien@hotmail.com <lmthien@hotmail.com>
2015-10-04 23:49:30 -0700

    update data schema

--- docs/orderfood.sql
+++ docs/orderfood.sql
@@ -9,7 +9,6 @@ CREATE TABLE `mtx_config` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-
 DROP TABLE IF EXISTS `mtx_user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
@@ -17,14 +16,14 @@ CREATE TABLE `mtx_user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(25) COLLATE utf8_unicode_ci NOT NULL,
   `fullname` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `salt` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
   `password` varchar(40) COLLATE utf8_unicode_ci NOT NULL,
   `email` varchar(60) COLLATE utf8_unicode_ci DEFAULT NULL,
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL,
-  `active` smallint(6) DEFAULT NULL,  
+  `active` smallint(6) DEFAULT NULL,
   `type` int(11) DEFAULT '1',
-  `active_token` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `created_id` int(11) DEFAULT NULL,
+  `updated_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
@@ -41,61 +40,9 @@ CREATE TABLE `mtx_language` (
   UNIQUE KEY `UNIQ_7A5F6FEF30F485F4` (`lang_key`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-DROP TABLE IF EXISTS `mtx_branch`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `mtx_branch` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `parent_id` int(11) DEFAULT NULL,  
-  `lft` int(11) DEFAULT NULL,
-  `rgt` int(11) DEFAULT NULL,
-  `status` tinyint(1) DEFAULT NULL,
-  `created_id` int(11) DEFAULT NULL,
-  `updated_id` int(11) DEFAULT NULL,
-  `created_at` datetime DEFAULT NULL,
-  `updated_at` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
-
-DROP TABLE IF EXISTS `mtx_branch_language`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `mtx_branch_language` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `language_id` int(11) DEFAULT NULL,
-  `branch_id` int(11) DEFAULT NULL,
-  `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
-CREATE TABLE IF NOT EXISTS `mtx_mediacategory` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `parent_id` int(11) DEFAULT NULL,
-  `created_id` int(11) DEFAULT NULL,
-  `updated_id` int(11) DEFAULT NULL,
-  `lft` int(11) DEFAULT NULL,
-  `rgt` int(11) DEFAULT NULL,
-  `status` tinyint(1) DEFAULT '0',
-  `is_static` tinyint(1) DEFAULT '0',
-  `created_at` datetime DEFAULT NULL,
-  `updated_at` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=16 ;
-
-
-CREATE TABLE IF NOT EXISTS `mtx_mediacategory_language` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `mediacategory_id` int(11) DEFAULT NULL,
-  `language_id` int(11) DEFAULT NULL,
-  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)  
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=15 ;
-
-
 CREATE TABLE IF NOT EXISTS `mtx_media` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `mediacategory_id` int(11) DEFAULT NULL,
+  `title` int(11) DEFAULT NULL,
   `created_id` int(11) DEFAULT NULL,
   `updated_id` int(11) DEFAULT NULL,
   `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
@@ -104,10 +51,8 @@ CREATE TABLE IF NOT EXISTS `mtx_media` (
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL,
   `active` tinyint(1) DEFAULT NULL,
-  `is_static` tinyint(1) DEFAULT '0',
-  `link` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  PRIMARY KEY (`id`)  
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=62 ;
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
 
 DROP TABLE IF EXISTS `mtx_product`;
@@ -117,18 +62,10 @@ CREATE TABLE `mtx_product` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `created_id` int(11) DEFAULT NULL,
   `updated_id` int(11) DEFAULT NULL,
-  `branch_id` int(11) DEFAULT NULL,
   `price` int(11) DEFAULT NULL,
-  `unit` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `discount` float DEFAULT NULL,
-  `thumb` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `amount` int(11) DEFAULT NULL,
   `status` tinyint(1) DEFAULT NULL,
-  `hot` tinyint(1) DEFAULT NULL,
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL,
-  `promotion` tinyint(1) DEFAULT NULL,
-  `promotion_price` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
@@ -147,41 +84,10 @@ CREATE TABLE `mtx_product_language` (
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 CREATE TABLE IF NOT EXISTS `mtx_product_media` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `products_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
   `media_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`product_id`,`media_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
-DROP TABLE IF EXISTS `mtx_comment`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `mtx_comment` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `products_id` int(11) DEFAULT NULL,
-  `created_id` int(11) DEFAULT NULL,
-  `updated_id` int(11) DEFAULT NULL,
-  `status` tinyint(1) NOT NULL,
-  `created_at` datetime NOT NULL,
-  `updated_at` datetime NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
-
-DROP TABLE IF EXISTS `mtx_comment_language`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `mtx_comment_language` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `language_id` int(11) DEFAULT NULL,
-  `comment_id` int(11) DEFAULT NULL,
-  `title` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `email` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `content` longtext COLLATE utf8_unicode_ci,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
 
 DROP TABLE IF EXISTS `mtx_orders`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -192,9 +98,9 @@ CREATE TABLE `mtx_orders` (
   `email` varchar(255) NOT NULL,
   `total` float DEFAULT NULL,
   `discount` int(11) DEFAULT NULL,
-  `customer_id` int(11) DEFAULT NULL,    
+  `customer_id` int(11) DEFAULT NULL,
   `note` text,
-  `shipping_info` longtext COLLATE utf8_unicode_ci,
+  `shipping_address` longtext COLLATE utf8_unicode_ci,
   `status` int(11) NOT NULL,
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL,
@@ -217,27 +123,29 @@ CREATE TABLE `mtx_order_detail` (
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 
-DROP TABLE IF EXISTS `mtx_daily_menu`;
+DROP TABLE IF EXISTS `mtx_weekly_menu`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `mtx_daily_menu` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,  
-  `note` text,  
+CREATE TABLE `mtx_weekly_menu` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `day_key` VARCHAR (3) DEFAULT NULL,
+  `day_title` VARCHAR (255) DEFAULT NULL,
+  `note` text,
   `status` int(11),
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL,
   `created_id` int(11) DEFAULT NULL,
   `updated_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 
-DROP TABLE IF EXISTS `mtx_daily_menu_detail`;
+DROP TABLE IF EXISTS `mtx_weekly_menu_detail`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `mtx_daily_menu_detail` (
+CREATE TABLE `mtx_weekly_menu_detail` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `daily_menu_id` int(11) NOT NULL,
-  `product_id` int(11) NOT NULL,  
+  `weekly_menu_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
