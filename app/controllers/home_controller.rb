f2c970c8e56d3d94e9213002b4ae1d7e4852bfbd
Scholaris Admin <sitadmin@Nams-Mac-mini.local>
2015-10-13 22:34:52 +1030

    menu for home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,15 @@
 class HomeController < ApplicationController
+  def index
+    @dayOfWeekList = {1 => "mon",
+                      2 => "tue",
+                      3 => "wed",
+                      4 => "thu",
+                      5 => "fri",
+                      6 => "sat",
+                      0 => "sun"
+    }
+    @today = @dayOfWeekList[Date.today.wday]
+    @weeklyMenuToday = MtxWeeklyMenu.find_by(["day_key = ?", @today])
+    @weeklyMenuDetailToday = MtxWeeklyMenuDetail.where("weekly_menu_id = ?", @weeklyMenuToday.id)
+  end
 end
--- app/models/mtx_weekly_menu_detail.rb
+++ app/models/mtx_weekly_menu_detail.rb
@@ -3,4 +3,5 @@ class MtxWeeklyMenuDetail < ActiveRecord::Base
 
     belongs_to :mtx_weekly_menu, :class_name => 'MtxWeeklyMenu', :foreign_key => :weekly_menu_id
     belongs_to :mtx_product, :class_name => 'MtxProduct', :foreign_key => :product_id
+    belongs_to :mtx_product_language, :class_name => 'MtxProductLanguage', :foreign_key => :product_id
 end
